d84dbbc5bd996bf41c7540aa6185841d0dc7ec85
shanmukha <nath.shanmukha@gmail.com>
2024-08-31 20:22:20 +0530

    fix

--- iam.tf
+++ iam.tf
@@ -1,12 +1,25 @@
-data "aws_iam_policy" "AmazonEKSServiceRolePolicy" {
-  name = "AmazonEKSServiceRolePolicy"
-}
+# Data resources for AWS-managed policies
 data "aws_iam_policy" "AmazonEKSClusterPolicy" {
   name = "AmazonEKSClusterPolicy"
 }
+
 data "aws_iam_policy" "AmazonEKSVPCResourceController" {
   name = "AmazonEKSVPCResourceController"
 }
+
+data "aws_iam_policy" "AmazonEKSWorkerNodePolicy" {
+  name = "AmazonEKSWorkerNodePolicy"
+}
+
+data "aws_iam_policy" "AmazonEC2ContainerRegistryReadOnly" {
+  name = "AmazonEC2ContainerRegistryReadOnly"
+}
+
+data "aws_iam_policy" "AmazonEKS_CNI_Policy" {
+  name = "AmazonEKS_CNI_Policy"
+}
+
+# EKS Cluster Role
 resource "aws_iam_role" "EKS_Cluster_Role" {
   name = "EKS_Cluster_Role"
   assume_role_policy = jsonencode({
@@ -18,20 +31,12 @@ resource "aws_iam_role" "EKS_Cluster_Role" {
         Principal = {
           Service = "eks.amazonaws.com"
         }
-      },
+      }
     ]
   })
 }
 
-resource "aws_iam_role_policy" "AmazonEKSServiceRolePolicy_attachment" {
-  name = "AmazonEKSServiceRolePolicy_attachment"
-  role = aws_iam_role.EKS_Cluster_Role.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = data.aws_iam_policy.AmazonEKSServiceRolePolicy.policy
-}
-
+# Policy attachments for EKS Cluster Role
 resource "aws_iam_policy_attachment" "AmazonEKSClusterPolicy_attachment" {
   name       = "AmazonEKSClusterPolicy_attachment"
   policy_arn = data.aws_iam_policy.AmazonEKSClusterPolicy.arn
@@ -45,17 +50,23 @@ resource "aws_iam_policy_attachment" "AmazonEKSVPCResourceController_attachment"
 }
 
 # Node group IAM Role
+resource "aws_iam_role" "AmazonEKSNodeRole" {
+  name = "AmazonEKSNodeRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-data "aws_iam_policy" "AmazonEKSWorkerNodePolicy" {
-  name = "AmazonEKSWorkerNodePolicy"
-}
-data "aws_iam_policy" "AmazonEC2ContainerRegistryReadOnly" {
-  name = "AmazonEC2ContainerRegistryReadOnly"
-}
-data "aws_iam_policy" "AmazonEKS_CNI_Policy" {
-  name = "AmazonEKS_CNI_Policy"
-}
-
+# Custom policy for EKS nodes to manage EBS volumes
 resource "aws_iam_policy" "create_delete_ebs_policy" {
   name        = "create_delete_ebs_policy"
   description = "Allows EKS nodes to create and delete EBS volumes"
@@ -67,34 +78,18 @@ resource "aws_iam_policy" "create_delete_ebs_policy" {
         Action = [
           "ec2:CreateVolume",
           "ec2:DeleteVolume",
-          "ec2:DescribeVolumes", // To list volumes before deleting
+          "ec2:DescribeVolumes",
           "ec2:AttachVolume",
-          "ec2:CreateTags",  // Optionally, for attaching
-          "ec2:DetachVolume" // Optionally, for detaching
+          "ec2:DetachVolume",
+          "ec2:CreateTags"
         ]
-        Resource = "*" // Or restrict to specific volume types/tags
-      },
+        Resource = "*"
+      }
     ]
   })
 }
 
-resource "aws_iam_role" "AmazonEKSNodeRole" {
-  name = "AmazonEKSNodeRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
+# Policy attachments for EKS Worker Node Role
 resource "aws_iam_policy_attachment" "AmazonEKSWorkerNodePolicy_attachment" {
   name       = "AmazonEKSWorkerNodePolicy_attachment"
   policy_arn = data.aws_iam_policy.AmazonEKSWorkerNodePolicy.arn
@@ -106,18 +101,15 @@ resource "aws_iam_policy_attachment" "AmazonEC2ContainerRegistryReadOnly_attachm
   policy_arn = data.aws_iam_policy.AmazonEC2ContainerRegistryReadOnly.arn
   roles      = [aws_iam_role.AmazonEKSNodeRole.name]
 }
+
 resource "aws_iam_policy_attachment" "AmazonEKS_CNI_Policy_attachment" {
   name       = "AmazonEKS_CNI_Policy_attachment"
   policy_arn = data.aws_iam_policy.AmazonEKS_CNI_Policy.arn
   roles      = [aws_iam_role.AmazonEKSNodeRole.name]
 }
+
 resource "aws_iam_policy_attachment" "eks_nodegroup_attachment" {
   name       = "eks_nodegroup_attachment"
   policy_arn = aws_iam_policy.create_delete_ebs_policy.arn
   roles      = [aws_iam_role.AmazonEKSNodeRole.name]
-}
-resource "aws_iam_policy_attachment" "eks_cluster_attachment" {
-  name       = "eks_cluster_attachment"
-  policy_arn = aws_iam_policy.create_delete_ebs_policy.arn
-  roles      = [aws_iam_role.EKS_Cluster_Role.name]
 }
