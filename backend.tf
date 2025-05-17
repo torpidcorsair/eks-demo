c6d746e3bfa0f8c054e07418e997675639730da0
torpidcorsair <arunofficialsmail@gmail.com>
2025-05-17 23:40:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "demo-app-tf-backend-bucket"
+    bucket = "demo-app-tf-backend-bucket-new"
     key    = "k8s_webapp_tf_state_file.tfstate"
     region = "ap-south-1"
   }
